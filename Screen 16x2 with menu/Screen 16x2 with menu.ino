901952a98f18c80ff77f130c228b28bdefb2a318
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-16 22:56:13 +0300

    code

--- Screen 16x2 with menu/Screen 16x2 with menu.ino
+++ Screen 16x2 with menu/Screen 16x2 with menu.ino
@@ -1,5 +1,6 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //импортируем библиотеку для работы с экраном
 
+// создаем пины
 constexpr uint8_t PIN_RS = 6;
 constexpr uint8_t PIN_EN = 7;
 constexpr uint8_t PIN_DB4 = 8;
@@ -7,22 +8,32 @@ constexpr uint8_t PIN_DB5 = 9;
 constexpr uint8_t PIN_DB6 = 10;
 constexpr uint8_t PIN_DB7 = 11;
 
-LiquidCrystal lcd();
+// создаем обьект lcd 
+LiquidCrystal lcd(PIN_RS, PIN_EN, PIN_DB4, 
+PIN_DB5, PIN_DB6, PIN_DB7);
 
-int name;
+int name; //создаем переменную считывающая данные
+int position = 0; //начальная позиция
 
-String ls[][3]= {
-  {"one", "two", "three"},
-  {"new", "update", "install"},
-  {"open", "close", "Play"}
+// создаем список основного меню
+String main-menu[][4]= {"number", "download", "start", "color"}
+// создаем вложенный массив команд из 4x5
+String command[][5]= {
+  {"one", "two", "three", "four", "back"},
+  {"new", "update", "install", "uninstall", "back"},
+  {"open", "close", "Play", "clear", "back"},
+  {"red", "green", "blue", "white", "black"}  
 };
 
+// один раз
 void setup() {
-  lcd.begin(16, 2);
-  Serial.begin(9600);
+  lcd.begin(16, 2); // определяем экран, как 2 строки на 16 столбцов
+  lcd.setCursor(0, 0); //ставим курсор на 0 столбец и первую строку
+  Serial.begin(9600); //заставляем общаться с монитором порта
+
 }
 
+// вечный цикл
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
