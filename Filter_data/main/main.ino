4d65316bca9c1d6f125bf788e7ceb622ddba2a7b
Voronov Nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-25 10:57:59 +0300

    new_varios

--- Filter_data/main/main.ino
+++ Filter_data/main/main.ino
@@ -1,30 +1,32 @@
-// Сетевой фильтр
+// Датчик расстояния без бибилиотек
+int trigPin = 12; 
+  int echoPin = 13;
 void setup(){
   Serial.begin(9600);
-  int trigPin = 12; 
-  int echoPin = 13;
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, OUTPUT);
   }
 
-void distance(){
+int dist(int trigPin, int echoPin){
   int duration, distance;
   // для большей точности установим значение LOW на пине Trig
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigPin, 0); 
   delayMicroseconds(2); 
   // Теперь установим высокий уровень на пине Trig
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin, 1);
   // Подождем 10 μs 
   delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigPin, 0); 
   // Узнаем длительность высокого сигнала на пине Echo
-  duration = pulseIn(echoPin, HIGH); 
+  duration = pulseIn(echoPin, 1); 
   // Рассчитаем расстояние
   distance = duration / 58;
   // Выведем значение в Serial Monitor
-  Serial.print(distance); 
-  Serial.println(" cm"); 
-  delay(100);
+  return distance;
   }
   
 void loop(){
-  
+  Serial.print(dist(trigPin, echoPin));
+  Serial.print(" cm");
+  delay(100);
   }
