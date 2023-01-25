53b8e4afccb059076b1104fb3b1e95e387397c65
Voronov Nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-25 10:43:37 +0300

    commit

--- Filter_data/main/main.ino
+++ Filter_data/main/main.ino
@@ -1,7 +1,30 @@
 // Сетевой фильтр
 void setup(){
-  
+  Serial.begin(9600);
+  int trigPin = 12; 
+  int echoPin = 13;
   }
+
+void distance(){
+  int duration, distance;
+  // для большей точности установим значение LOW на пине Trig
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2); 
+  // Теперь установим высокий уровень на пине Trig
+  digitalWrite(trigPin, HIGH);
+  // Подождем 10 μs 
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW); 
+  // Узнаем длительность высокого сигнала на пине Echo
+  duration = pulseIn(echoPin, HIGH); 
+  // Рассчитаем расстояние
+  distance = duration / 58;
+  // Выведем значение в Serial Monitor
+  Serial.print(distance); 
+  Serial.println(" cm"); 
+  delay(100);
+  }
+  
 void loop(){
   
   }
