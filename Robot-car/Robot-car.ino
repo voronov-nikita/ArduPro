d768a83ed115e4aac2495fd4f6d20784c12c1505
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-27 21:15:45 +0300

    update

--- Robot-car/Robot-car.ino
+++ Robot-car/Robot-car.ino
@@ -5,6 +5,15 @@
 #define motor1 = 7
 #define motor2 = 8
 
+
+float k = 0.1; // коэффицент погрешности, настраиваемый
+// сетевой фильтр
+float expRunningAverage(float newVal){
+  static float filVal = 0;
+  filVal+=(newVal-filVal)*k;
+  return filVal;
+  }
+
 // Датчик растояния
 int dist(int trigPin, int echoPin){
   int duration, distance;
