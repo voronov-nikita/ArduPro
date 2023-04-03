abc1a92e7d1e7c787582447a56ddd9e78238d185
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-04-03 18:02:57 +0300

    Update main.ino

--- greenhouse/main/main.ino
+++ greenhouse/main/main.ino
@@ -4,7 +4,7 @@
 #define dw digitalWrite
 #define ar analogRead
 
-int rate = 327;
+int rate = 0;
 int stoped = 60;
 
 int list_pin_read[6] = {A0, A1, A2, A3, A4, A5};
@@ -20,7 +20,7 @@ void setup() {
   for (int i=7; i<=12; i++) pinMode(i, OUTPUT);   
   
   // определение пинов для воздуха
-  for (int i=1; i<1+4; i++){
+  for (int i=2; i<2+4; i++){
     pinMode(i, INPUT);
     dw(i, 1);
   }    
@@ -29,26 +29,34 @@ void setup() {
 void loop() {
   // формирование списка со значениями датчика почвы
   for (int i=7; i<7+6; i++) list_soil_values[i-7] = new_data_sensor(i, list_pin_read[i-7]);
+  Serial.println(list_soil_values[0]);
+  Serial.print(list_soil_values[1]);
+  Serial.println(list_soil_values[2]);
+  delay(500);
+  
   // формирование списка со значениями температуры и влажности воздуха
-  for (int pin=1; pin < 1+4; pin++){
+  for (int pin=2; pin < 2+4; pin++){
     // считываем значения
     int temperature = sensor.readTemperature(pin);
     int humidity = sensor.readHumidity(pin);
-
-    // аполняем список
+    delay(10);
+    // Serial.print(temperature);
+    // Serial.println(humidity);
+    // заполняем список
     list_air_values[pin-1][0] = temperature;
     list_air_values[pin-1][1] = humidity;
   }
+
   // задержка при опрашивании датчика в <часах>
-  delay(stoped*1000*60*60);                                                        
+  // delay(stoped*1000*60*60);                                                        
 }
 
 // опрашивание датчика почвы используя пин питания и пин чтения
 int new_data_sensor(int sensorwrite, int sensorread){
-  dw(8, 1);
+  dw(sensorwrite, 1);
   delay(10);
     
   int sensor_value = ar(sensorread);
   dw(sensorwrite, 0);
-  return map(sensor_value, rate, 1023, 100, 0);
+  return map(sensor_value, rate, 1023, 0, 100);
 }
